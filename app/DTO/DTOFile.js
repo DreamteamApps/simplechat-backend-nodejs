03b775e3b891a8b84ff3f8c11d76ed469b75c04c
Ricardo Vaz Correa <ricardovcorrea@live.com>
2020-05-10 23:31:44 -0300

    Returning duration in audio files fix

--- app/DTO/DTOFile.js
+++ app/DTO/DTOFile.js
@@ -1,11 +1,11 @@
 class DTOFile {
     constructor(fileModel) {
-        const { id, name, extension, duration, url, thumbnail_url } = fileModel;
+        const { id, name, extension, duration_seconds, url, thumbnail_url } = fileModel;
 
         this.id = id;
         this.name = name;
         this.extension = extension;
-        this.duration = duration;
+        this.duration = duration_seconds;
         this.url = url;
         this.thumbnailUrl = thumbnail_url;
     }
